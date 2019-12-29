9c4c58063e81b5656c33ec99945720d0136e86c8
srinivasboga7 <cs17btech11009@iith.ac.in>
2019-12-29 18:10:59 +0530

    Revert "changed the discovery to UDP"
    This reverts commit 74178728d728597ef413a49b2f6b5c318506c493.

--- Discovery/discovery.go
+++ Discovery/discovery.go
@@ -3,17 +3,13 @@ package Discovery
 import (
 	//"io/ioutil"
 	//"strings"
-	"bytes"
-	"log"
 	"net"
-	"strconv"
 	//dt "GO-DAG/DataTypes"
 	//"log"
-	"encoding/binary"
-	"encoding/json"
 	"fmt"
-	"strings"
 	"time"
+	"encoding/json"
+	"strings"
 )
 
 type Request struct {
@@ -21,28 +17,23 @@ type Request struct {
 	NodeType string
 }
 
-type peerAddr struct {
-	IP        []byte
-	PublicKey []byte
-}
-
 func GetIps(Addrs string) []string {
 	//Returns slice which has IPs as strings
 	//Currently getting from file
 	//Later get from DNS server or anything else
 	tcpAddr, _ := net.ResolveTCPAddr("tcp4", Addrs)
-	conn, _ := net.DialTCP("tcp", nil, tcpAddr)
+	conn,_ := net.DialTCP("tcp",nil,tcpAddr)
 	var req Request
 	req.NodeType = "StorageNode"
-	request, err := json.Marshal(req)
+	request,err := json.Marshal(req)
 	if err != nil {
 		fmt.Println(err)
 	}
 	conn.Write(request)
-	buf := make([]byte, 1024)
-	l, _ := conn.Read(buf)
+	buf := make([]byte,1024)
+	l,_ := conn.Read(buf)
 	var IPList []string
-	err = json.Unmarshal(buf[:l], &IPList)
+	err = json.Unmarshal(buf[:l],&IPList)
 	if err != nil {
 		fmt.Println(err)
 	}
@@ -50,15 +41,15 @@ func GetIps(Addrs string) []string {
 	return IPList
 }
 
-func ConnectToServer(ips []string) map[string]net.Conn {
+func ConnectToServer(ips []string) (map[string] net.Conn) {
 	//Takes a map of ips to socket file descriptors of tcp connections
-	p := make(map[string]net.Conn)
-	for _, ip := range ips {
+	p := make(map[string] net.Conn)
+	for _,ip := range ips {
 		tcpAddr, _ := net.ResolveTCPAddr("tcp4", ip)
 		for {
-			conn, err := net.DialTCP("tcp", nil, tcpAddr) //BLocking call
+			conn,err := net.DialTCP("tcp", nil, tcpAddr)    //BLocking call
 			if err == nil {
-				ip = ip[:strings.IndexByte(ip, ':')]
+				ip = ip[:strings.IndexByte(ip,':')]
 				p[ip] = conn
 				break
 			}
@@ -66,55 +57,4 @@ func ConnectToServer(ips []string) map[string]net.Conn {
 		}
 	}
 	return p
-}
-
-func queryDiscoveryService(localAddr string, pubKey []byte, servAddr string) []peerAddr {
-	// serializing the IP address and constructing the query
-	ip := strings.Split(localAddr, ".")
-	query := []byte{0x03}
-	for _, n := range ip {
-		buf := new(bytes.Buffer)
-		num, _ := strconv.Atoi(n)
-		binary.Write(buf, binary.LittleEndian, uint8(num))
-		query = append(query, buf.Bytes()...)
-	}
-	query = append(query, pubKey...)
-
-	// querying the discovery service
-
-	udpAddr, _ := net.ResolveUDPAddr("udp4", servAddr)
-	conn, err := net.DialUDP("udp", nil, udpAddr)
-	if err != nil {
-		log.Fatal("Discovery Service Down")
-	}
-	conn.WriteToUDP(query, udpAddr)
-	buf := make([]byte, 1024)
-
-	// response from discovery service
-	l, _, _ := conn.ReadFromUDP(buf)
-	var peers []peerAddr
-	json.Unmarshal(buf[:l], &peers)
-
-	// sending the acknowledgment
-	conn.WriteToUDP([]byte{0x05}, udpAddr)
-
-	return peers
-}
-
-func udpServer() {
-	addr, _ := net.ResolveUDPAddr("udp4", ":8080")
-	for {
-		conn, err := net.ListenUDP("udp", addr)
-		if err != nil {
-			fmt.Println(err)
-			conn.Close()
-			continue
-		}
-		buf := make([]byte, 1)
-		_, dest, _ := conn.ReadFromUDP(buf)
-		if buf[0] == 0x01 {
-			conn.WriteToUDP([]byte{0x02}, dest)
-		}
-		conn.Close()
-	}
 }
