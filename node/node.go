fe6bb10659bd697c1d531dc114d7649d54e18873
srinivasboga7 <cs17btech11009@iith.ac.in>
2020-04-28 16:25:32 +0530

    fixed sharding issues

--- node/node.go
+++ node/node.go
@@ -78,13 +78,6 @@ func handleMsg(msg p2p.Msg, send chan p2p.Msg, p *p2p.Peer, ShardSignalch chan d
 		}
 		replyMsg.LenPayload = uint32(len(replyMsg.Payload))
 		p.Send(replyMsg)
-	} else if msg.ID == 35 { //Shard signal
-		signal, _ := serialize.Decode35(msg.Payload, msg.LenPayload)
-		select {
-		case ShardSignalch <- signal:
-			send <- msg
-		default:
-		}
 	} else if msg.ID == 33 {
 		// reply to a request sync transaction
 		tx, sign := serialize.Decode32(msg.Payload, msg.LenPayload)
@@ -105,12 +98,6 @@ func handleMsg(msg p2p.Msg, send chan p2p.Msg, p *p2p.Peer, ShardSignalch chan d
 				}
 			}
 		}
-	} else if msg.ID == 36 {
-		tx, _ := serialize.Decode36(msg.Payload, msg.LenPayload)
-		// if sh.VerifyShardTransaction(tx, sign, 4) {
-		// 	Shardtxch <- tx
-		// }
-		Shardtxch <- tx
 	}
 }
 
